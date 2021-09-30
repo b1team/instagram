c4b4c96580c23887512f4d4996a00b0e997acbac
vuonglv1612 <it.vuonglv@gmail.com>
2021-09-30 23:58:02 +0700

    feat/added_instagram_login

--- instagram_download/worker/jobs/tasks.py
+++ instagram_download/worker/jobs/tasks.py
@@ -1,12 +1,15 @@
 from instagram_download.worker.worker import celery_app
 from instagram_download.worker.jobs.instagram import InstagramScaper
+from instagram_download.settings import InstagramUserConfig
 
 instagram = InstagramScaper()
+instagram.login(InstagramUserConfig.login_username, InstagramUserConfig.login_password)
 
 @celery_app.task
 def insta_media_wrapper(url):
 	media = instagram.get_media(url)
 	return media
+
 
 @celery_app.task
 def instagram_url_wrapper(media):

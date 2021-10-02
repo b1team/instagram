4835ae0fe0c3afd733d178642a9c192ee6f9d0e0
vuonglv1612 <it.vuonglv@gmail.com>
2021-10-02 11:07:38 +0700

    feat: added Makfile, linting, formating, entrypoint into project

--- instagram_download/celery_app.py
+++ instagram_download/celery_app.py
@@ -1,5 +1,4 @@
 from celery import Celery
-from multiprocessing import Process
 
 from instagram_download.settings import CeleryConfig
 
@@ -7,16 +6,3 @@ celery_app = Celery("tasks",
                     broker=CeleryConfig.broker_url,
                     include=["instagram_download.worker.jobs.tasks"],
                     backend=CeleryConfig.backend_url)
-
-
-def worker():
-    w = celery_app.Worker(
-        loglevel="INFO",
-        concurrency=2
-    )
-    w.start()
-
-
-if __name__ == "__main__":
-    w = Process(target=worker)
-    w.start()
